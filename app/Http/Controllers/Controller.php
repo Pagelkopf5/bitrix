e3c9c27966681118a8b670b3299556671faf4504
Gabriel Pagelkopf <gabriel.pagelkopf@gmail.com>
2025-01-06 22:16:52 -0300

    add bitrix connection

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Connection\Bitrix;
+
 abstract class Controller
 {
-    //
+    public function verify(Request $request)
+    {
+        return response('', 302)->header('Location', env('FRONTEND_URL'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+
+Route::post('/', [Controller::class, 'verify']);
 
 Route::get('/', function () {
     return 'Hello World';
